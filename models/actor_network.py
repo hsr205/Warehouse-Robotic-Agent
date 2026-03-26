90680637a18be7942be2db03b06b21692c253d02
Henry Rothenberg <hsr205@aol.com>
2026-03-26 10:30:36 -0400

    WIP

--- models/actor_network.py
+++ models/actor_network.py
@@ -16,22 +16,34 @@ class ActorNetwork(nn.Module):
         )
 
         self._device = device
+        self.to(self._device)
 
-    def forward(self, observation) -> Tensor:
+    def forward(self, observation_tensor:Tensor) -> Tensor:
 
-        if isinstance(observation, np.ndarray):
-            observation = torch.tensor(observation, dtype=torch.float, device=self._device)
+        if isinstance(observation_tensor, np.ndarray):
+            observation_tensor = torch.tensor(observation_tensor, dtype=torch.float, device=self._device)
 
-        if observation.dim() == 1:
+        print("Here")
+
+        if observation_tensor.dim() == 1:
             # Add a new dimension to the observation at a specified location
-            observation = observation.unsqueeze(0)
+            observation_tensor = observation_tensor.unsqueeze(0)
+
+        print("Now Here")
 
         # Once the observation is valid,
         # Pass the observation through the network and return raw logits
-        logits = self._actor_network(observation)
+        # print(f"observation = {observation}")
+        # print(f"type(observation) = {type(observation)}")
+
+        logits = self._actor_network(observation_tensor)
+
+        print("After logits")
 
         # Using SoftMax, convert the logits into action probabilities
         action_probabilities_tensor: Tensor = torch.softmax(logits, dim=-1)
+
+        print("End of forward() pass method")
 
         return action_probabilities_tensor
 
