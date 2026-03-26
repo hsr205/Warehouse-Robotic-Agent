572b469de97f624f4a6786294ce7a30a05b469bf
Henry Rothenberg <hsr205@aol.com>
2026-03-26 17:23:47 -0400

    Replace regular FNN with a CNN / FNN implementation in the actor_network.py module

--- models/actor_network.py
+++ models/actor_network.py
@@ -1,49 +1,59 @@
-import numpy as np
 import torch
 from torch import nn, Tensor
 
 
 class ActorNetwork(nn.Module):
-    def __init__(self, input_dimensions: int, output_dimensions: int, device):
+    def __init__(self, output_dimensions: int, device):
         super(ActorNetwork, self).__init__()
 
-        self._actor_network = nn.Sequential(
-            nn.Linear(in_features=input_dimensions, out_features=32),
+        # NOTE: We leverage a convolutional neural network because our observations are
+        #       actually pixels that represent our grid environment
+        self._convolutional_neural_network = nn.Sequential(
+
+            # NOTE: in_channels is required to be 3 because RGB only has 3 channels
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Linear(in_features=32, out_features=32),
+
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Linear(in_features=32, out_features=output_dimensions),
+        )
+
+        self._fully_connected_neural_network = nn.Sequential(
+            # NOTE: in_features is a product of the out_channels of the
+            #       previous CNN layer and the number of discrete actions
+            nn.Linear(in_features=32 * 7 * 7, out_features=128),
+            nn.ReLU(),
+
+            nn.Linear(in_features=128, out_features=output_dimensions),
         )
 
         self._device = device
         self.to(self._device)
 
-    def forward(self, observation_tensor:Tensor) -> Tensor:
+    def forward(self, observation_tensor: Tensor) -> Tensor:
 
-        if isinstance(observation_tensor, np.ndarray):
-            observation_tensor = torch.tensor(observation_tensor, dtype=torch.float, device=self._device)
+        observation_tensor = observation_tensor.to(self._device, dtype=torch.float32)
 
-        print("Here")
+        # NOTE: For this normalization, the value, 255.0, is
+        #       leveraged as that is the maximum value that a pixel can hold
+        observation_tensor = observation_tensor / 255.0
 
-        if observation_tensor.dim() == 1:
-            # Add a new dimension to the observation at a specified location
+        # NOTE: The shape must be changed from (Height, Width, Channel) → (Channel, Height, Width)
+        if observation_tensor.dim() == 3:
+            # NOTE: permute reorders the dimensions of a tensor
+            observation_tensor = observation_tensor.permute(2, 0, 1)
+
+        if observation_tensor.dim() == 3:
+            # NOTE: unsqueeze adds a dimension to a tensor
             observation_tensor = observation_tensor.unsqueeze(0)
 
-        print("Now Here")
+        x = self._convolutional_neural_network(observation_tensor)
 
-        # Once the observation is valid,
-        # Pass the observation through the network and return raw logits
-        # print(f"observation = {observation}")
-        # print(f"type(observation) = {type(observation)}")
+        # Note: Flattens the tensor to something digestible for the FNN
+        x_flatten = x.view(x.size(0), -1)
 
-        logits = self._actor_network(observation_tensor)
+        logits = self._fully_connected_neural_network(x_flatten)
 
-        print("After logits")
-
-        # Using SoftMax, convert the logits into action probabilities
         action_probabilities_tensor: Tensor = torch.softmax(logits, dim=-1)
 
-        print("End of forward() pass method")
-
         return action_probabilities_tensor
-
