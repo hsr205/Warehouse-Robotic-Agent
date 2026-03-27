62356f8569aa641b8319fad416079492d02bcd73
Henry Rothenberg <hsr205@aol.com>
2026-03-27 15:32:03 -0400

    Add demo logic to main.py module

--- main/main.py
+++ main/main.py
@@ -2,18 +2,21 @@ from logging import Logger
 
 from logger.logger import AppLogger
 from models.warehouse_agent_ppo import WareHouseAgentPPO
+from models.warehouse_agent_ppo_evaluation import WareHouseAgentPPOEvaluation
 from warehouse_env.warehouse_env import WareHouseEnv
 
 
 def main() -> int:
     logger: Logger = AppLogger().get_logger(__name__)
     warehouse_agent_ppo: WareHouseAgentPPO = WareHouseAgentPPO()
+    warehouse_agent_ppo_evaluation: WareHouseAgentPPOEvaluation = WareHouseAgentPPOEvaluation()
     warehouse_env: WareHouseEnv = WareHouseEnv()
 
     try:
 
         warehouse_env.randomly_navigate_custom_grid_world()
         # warehouse_agent_ppo.train_agent()
+        # warehouse_agent_ppo_evaluation.evaluate_agent()
 
         return 0
 
