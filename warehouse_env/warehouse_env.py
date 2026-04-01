fca85344ea6ecfd8beade7b404d5d36f08318128
Henry Rothenberg <hsr205@aol.com>
2026-04-01 18:26:04 -0400

    Expand grid env in warehouse_env.py module

--- warehouse_env/warehouse_env.py
+++ warehouse_env/warehouse_env.py
@@ -15,7 +15,7 @@ from logger.logger import AppLogger
 class WareHouseEnv(MiniGridEnv):
     def __init__(
             self,
-            size: int = 10,
+            size: int = 12,
             agent_start_position_tuple: tuple[int, int] = (1, 1),
             agent_start_direction: int = 0,
             max_steps: int | None = None,
