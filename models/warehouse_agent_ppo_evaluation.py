a6680cffe86f373095a71e2275526b27da96e1c9
Henry Rothenberg <hsr205@aol.com>
2026-04-08 09:23:31 -0400

    Alter constructor logic in warehouse_agent_ppo_evaluation.py module

--- models/warehouse_agent_ppo_evaluation.py
+++ models/warehouse_agent_ppo_evaluation.py
@@ -14,28 +14,16 @@ from logger.logger import AppLogger
 from models.actor_network import ActorNetwork
 from models.critic_network import CriticNetwork
 from warehouse_env.warehouse_env import WareHouseEnv
+from warehouse_env.warehouse_env_2 import WareHouseEnv2
 from warehouse_env.warehouse_env_3 import WareHouseEnv3
 
 
 class WareHouseAgentPPOEvaluation:
 
-    def __init__(self) -> None:
+    def __init__(self, environment_obj: WareHouseEnv | WareHouseEnv2 | WareHouseEnv3) -> None:
         self._learning_rate: float = 3e-4
-        self._environment_obj: Env = WareHouseEnv(render_mode=None)
+        self._environment_obj = environment_obj
         self._logger = AppLogger.get_logger(self.__class__.__name__)
-        # self._environment_obj: WareHouseEnv = WareHouseEnv(render_mode='none')
-        # self._environment_obj_human_render_mode: WareHouseEnv = WareHouseEnv(render_mode='human')
-
-        # self._environment_obj: WareHouseEnv2 = WareHouseEnv2(render_mode='none')
-        # self._environment_obj_human_render_mode: WareHouseEnv2 = WareHouseEnv2(render_mode='human')
-
-        self._environment_obj: WareHouseEnv3 = WareHouseEnv3(render_mode='none')
-        self._environment_obj_human_render_mode: WareHouseEnv3 = WareHouseEnv3(render_mode='human')
-
-        # # TODO: Uncomment after testing
-        # self._action_dimensions = self._environment_obj.action_space.n
-
-        # TODO: Remove after testing
         self._action_dimensions = self._environment_obj.action_space = Discrete(4).n
 
         self._device = self._get_device()
